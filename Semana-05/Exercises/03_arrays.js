87e266060ae86fe0d0d0d108e6fda1642b90a68f
JulaSpa <julianspataro@hotmail.com>
2022-04-14 17:06:11 -0300

    BaSP-2022: Arrays exercise done

---
+++ Semana-05/Exercises/03_arrays.js
@@ -0,0 +1,45 @@
+//Exercise "A"
+
+array = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+console.log(array[4] + " " + array[10])
+
+//Exercise "B"
+
+array2 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+console.log(array2.sort())
+
+//Exercise "C"
+
+var array3 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+var unshift = array3.unshift("First element")
+var push = array3.push("Final element");
+
+console.log(array3)
+
+//Exercise "D"
+
+var array4 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+var shift = array4.shift()
+var pop = array4.pop()
+
+console.log(array4)
+
+//Exercise "E"
+
+var array5 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+console.log(array5.reverse())
+
+//Exercise "F"
+
+var array6 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+console.log(array6.join('-'))
+
+//Exercise "G"
+
+var array7 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+console.log(array7.slice(4, 11))
