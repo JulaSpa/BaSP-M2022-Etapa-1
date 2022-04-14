bfaebb028bc56e9fa29f63e2b69dc1b6d289a01b
JulaSpa <julianspataro@hotmail.com>
2022-04-14 18:16:52 -0300

    BaSP-2022: For exercise done

---
+++ Semana-05/Exercises/05_for.js
@@ -0,0 +1,45 @@
+//Exercise "A"
+
+ var array = ["enero", "febrero", "marzo", "abril", "mayo"]
+
+ for (arr of array){
+
+    
+
+    alert(arr)
+}
+
+//Exercise "B"
+
+var array2 = ["enero", "febrero", "marzo", "abril", "mayo"]
+
+for (arr2 of array2){
+
+    
+
+    alert(arr2[0].toUpperCase() + arr2.substring(1))
+} 
+
+//Exercise "C"
+
+var sentence = ""
+
+for (i in array){
+
+  var newArr = array
+
+}
+
+var stringArr = newArr.join('-')
+
+sentence=stringArr
+
+alert(sentence) 
+
+//Exercise "D"
+
+array3=[]
+
+for (array3=0; array3<10; array3++){
+    console.log(array3)
+}
