3cb044d9c2700bf95fb6d77204b3fe4262c840f4
JulaSpa <julianspataro@hotmail.com>
2022-04-14 15:44:34 -0300

    BaSP-2022: Excercise 01 done

---
+++ Semana-05/Exercises/01_variables_and_operators.js
@@ -0,0 +1,23 @@
+//Exercise "A"
+
+var num1 = 10;
+var num2 = 5;
+var res = num1 + num2; 
+
+console.log(res)
+
+//Exercise "B"
+
+var name = 'Jon'
+var lastName = 'Snow'
+var hello = `Hello ${name} ${lastName}`;
+
+console.log(hello)
+
+//Exercise "C"
+
+var text1 = "Ricardo"
+var text2 = "Esteban"
+var sumString = text1.length + text2.length;
+
+console.log(sumString)
